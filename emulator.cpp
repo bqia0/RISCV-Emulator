315d825db43aa5eaf419341182287228b44c8461
Nick Leung <leung5326@gmail.com>
2019-05-13 19:25:09 -0500

    moved file parsing to its own function

--- emulator.cpp
+++ emulator.cpp
@@ -5,34 +5,43 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cout << "Please specify a machine code file." << endl;
-        return 0;
-    }
-
-    ifstream machineFile(argv[1], ios::in|ios::binary|ios::ate);
+char* readFileContents(char* filepath) {
+    ifstream machineFile(filepath, ios::in|ios::binary|ios::ate);
     if (machineFile.is_open()) {
 
         // read file into memory
         streampos fileSize = machineFile.tellg();
-        char* fileContents = new char[fileSize];
+        char* buffer = new char[fileSize];
         machineFile.seekg(0, ios::beg);
-        machineFile.read(fileContents, fileSize);
+        machineFile.read(buffer, fileSize);
         machineFile.close();
 
         cout << "File has been read into memory" << endl;
 
         // verification that its reading correctly?
         for (int i = 0; i < fileSize; i++) {
-            // cout << fileContents[i] << ' ';
-            cout << hex << setfill('0') << setw(2) << fileContents[i] << " ";
+            cout << setw(2) << setfill('0') << hex << (int) buffer[i] << " "; 
         }
         cout << endl;
+        return buffer;
+    } else {
+        throw invalid_argument("file could not be opened");
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Please specify a machine code file." << endl;
+        return 0;
+    }
+
+    try {
+        char* fileContents = readFileContents(argv[1]);
 
         delete[] fileContents;
-    } else {
-        cout << "The specified file could not be opened." << endl;
+    } catch (invalid_argument& e) {
+        cout << "The specified file could not be opened";
+        return 0;
     }
-    return 0;
+
 }
